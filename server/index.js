ee9530beca160e3c92cf810bbb8c63ddb0eee860
morshedulmunna <morshedulmunna1@gmail.com>
2022-04-28 03:02:00 +0600

    Database connect

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+
+// require=>>
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -9,3 +14,13 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+// DB Connection
+const connectUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.db01v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(connectUrl)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is Running on Port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
